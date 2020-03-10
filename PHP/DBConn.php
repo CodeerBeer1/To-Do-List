807461bcaf5da6f87d1279f3e0b32d1b7da40f4f
Khizer Butt <99053182@mydavinci.nl>
2020-03-10 19:17:44 +0100

    .

--- PHP/DBConn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $username = "usn";
-    $password = "pswrd";
-
-    $conn = "mysql:host=localhost;db=test",$username, $password;
-    
-
-?>
--- PHP/Home.php
+++ PHP/Home.php
@@ -1,6 +1,31 @@
 <?php
 
-include "DBConn.php"
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "list";
 
+$dsn = 'mysql:host='.$server.';dbname='.$database;
+$pdo = new PDO($dsn, $username, $password);
+try
+{
+    $pdo = new PDO($dsn, $username, $password);
+    echo "connectie gemaakt";
+}
+
+catch(PDOException $e)
+{
+    print $e->getMessage();
+}
+
+$sql = "INSERT INTO benodigdheden (de ding) VALUES (?, ?)";
+$statement = $pdo->prepare($sql);
+$statement->execute(array("auto", "wiel"));
 
 ?>
+
+<h1>
+
+
+
+</h1>
